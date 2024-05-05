42c971931c9d1f1c4dd7f9ccb938394aa8fb6a24
gabrielleles18 <gabriel.leles18@gmail.com>
2024-05-05 09:29:16 -0300

    Initial configs

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,17 +9,17 @@ export const GlobalStyle = createGlobalStyle`
 
   :focus {
     outline: none;
-    box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
+    box-shadow: 0 0 0 2px ${(props) => props.theme['yellow-dark']};
   }
 
   body {
-    background: ${(props) => props.theme['gray-900']};
-    color: ${(props) => props.theme['gray-300']};
+    background: ${(props) => props.theme["background"   ]};
+    color: ${(props) => props.theme['base-text']};
     -webkit-font-smoothing: antialiased;
   }
 
   body, input, textarea, button {
-    font-family: 'Roboto', sans-serif;
+    font-family: "Baloo 2", sans-serif;
     font-weight: 400;
     font-size: 1rem;
   }
